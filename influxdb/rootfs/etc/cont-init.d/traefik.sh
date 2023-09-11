b5582b7bfa861e42cc54a3302eeef3c7f24bb214
root <root@a0d7b954-vscode.local.hass.io>
2023-09-11 19:31:34 +0200

    Switched to traefik to rewrite response

---
+++ influxdb/rootfs/etc/cont-init.d/traefik.sh
@@ -0,0 +1,28 @@
+#!/command/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: InfluxDBv2
+# Configures Traefik for use with the InfluxDB v2
+# ==============================================================================
+declare port
+declare certfile
+declare dns_host
+declare ingress_interface
+declare ingress_port
+declare ingress_entry
+declare keyfile
+
+certfile=$(bashio::config 'certfile')
+keyfile=$(bashio::config 'keyfile')
+ingress_port=$(bashio::addon.ingress_port)
+port=$(bashio::addon.port 8086)
+ingress_interface=$(bashio::addon.ip_address)
+ingress_entry=$(bashio::addon.ingress_entry)
+
+sed -i "s#%%certfile%%#${certfile}#g" /etc/traefik/traefik.yaml
+sed -i "s#%%keyfile%%#${keyfile}#g" /etc/traefik/traefik.yaml
+
+sed -i "s/%%ingress_port%%/${ingress_port}/g" /etc/traefik/traefik.yaml
+sed -i "s/%%port%%/${port}/g" /etc/traefik/traefik.yaml
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/traefik/traefik.yaml
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/traefik/traefik.yaml
+
