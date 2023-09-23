22a6773bca1617136cf55c5ba41aa62ea2d23f8e
Daniel Oldberg <contact@danieloldberg.se>
2023-09-23 20:30:15 +0000

    Added migration script from v1 to v2

---
+++ migrate_v1_to_v2.sh
@@ -0,0 +1,11 @@
+docker exec -it {{ addon_xxxxx_influxdb }} /bin/bash
+influx_inspect export -database homeassistant -retention autogen -out /data/exports/influxdb -lponly -datadir /data/influxb -waldir /data/influxdb/wal
+exit
+docker cp {{ addon_xxxxx_influxdb }}:/data/exports/influxdb /root/
+docker cp /root/influxdb {{ addon_xxxxx_influxdbv2 }}:/data/imports
+docker exec -it {{ addon_xxxxx_influxdbv2 }} /bin/bash
+influx write \
+  --org-id homeassistant \
+  --bucket homeassistant \
+  --file /data/imports \
+  --token {{ TOKEN }}
